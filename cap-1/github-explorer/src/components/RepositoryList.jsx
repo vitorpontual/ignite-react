c6936eca409185cb7e2c4ef551975e48204488a0
Vitor Pontual <vitorpguedes@me.com>
2021-06-02 19:54:06 -0300

    Using Api

--- cap-1/github-explorer/src/components/RepositoryList.jsx
+++ cap-1/github-explorer/src/components/RepositoryList.jsx
@@ -1,30 +1,32 @@
-import { RepositoryItem } from "./RepositoryItem"
+import { useEffect, useState } from 'react';
+import { RepositoryItem } from "./RepositoryItem";
 
+import "../styles/repository.scss"
 
-const repository = {
-  name: "unform",
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
+// https://api.github.com/users/vitorpontual/repos
 
 export function RepositoryList() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/vitorpontual/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, []); // When array was empty, change one time
+
+
   return (
     <section className="repository-list">
       <h1>Lista de Repositorios</h1>
 
       <ul>
-        <RepositoryItem
-          repository={repository}
-        />
-        <RepositoryItem
-          repository={repository}
-        />
-        <RepositoryItem
-          repository={repository}
-        />
-        <RepositoryItem
-          repository={repository}
-        />
+        {
+          repositories.map(repository => {
+            return <RepositoryItem key={repository.name} repository={repository} />
+          })
+        }
+
       </ul>
     </section>
   )

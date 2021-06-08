a4f2cb33ad73a5167c9a15048249375ac8b16fe8
Vitor Pontual <vitorpguedes@me.com>
2021-06-07 21:50:28 -0300

    Modals and Forms part.1

--- cap-2/vpmoney/src/components/Header/index.tsx
+++ cap-2/vpmoney/src/components/Header/index.tsx
@@ -1,15 +1,19 @@
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from './styles'
 
-export function Header() {
-  return (
+
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
+    return (
     <Container>
       <Content>
         <img src={logoImg} alt="vp money" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionModal}>
           Nova transação
-      </button>
-
+        </button>
+        
       </Content>
     </Container>
   )

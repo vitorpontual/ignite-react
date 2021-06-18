2f9959938a011352d325d6ece6879ac4f97f4e7e
Vitor Pontual <vitorpguedes@me.com>
2021-06-18 11:43:05 -0300

    Component Header and SignButton Styles

--- cap-3/ignews/src/pages/_document.tsx
+++ cap-3/ignews/src/pages/_document.tsx
@@ -8,12 +8,14 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500;700;900&display=swap" rel="stylesheet" />
 
-       </Head>
+          <link rel="shortcut icon" href="/favic on.png" type="image/png" />
+
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-          )
+    )
   }
 }

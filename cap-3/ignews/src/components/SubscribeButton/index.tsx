431d966d482f495454db5ff56edf52e07e64b815
Vitor Pontual <vitorpguedes@me.com>
2021-06-20 15:48:10 -0300

    Checkout Page and validation

--- cap-3/ignews/src/components/SubscribeButton/index.tsx
+++ cap-3/ignews/src/components/SubscribeButton/index.tsx
@@ -1,14 +1,49 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
   priceId: string
 }
 
+// Guardar Credenciais Secretas
+// (SSR)
+// (SSG)
+// *API routes 
+
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe(){
+    if(!session) {
+      signIn('github');
+      return
+    }
+    // criação da checkout
+
+    try { 
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+           
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({sessionId})
+
+    }catch(err){
+      alert(err.message)
+    }
+  }
   return (
     <button
       type='button'
       className={styles.subscribeButton}
-    >Subscribe Now</button>
+      onClick={handleSubscribe}
+    >
+      Subscribe Now
+    </button>
   );
 }
--- cap-3/ignews/src/pages/api/auth/[...nextauth].ts
+++ cap-3/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
+import { signIn } from 'next-auth/client';
 import Providers from 'next-auth/providers';
+import { FaUntappd } from 'react-icons/fa';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -7,9 +11,47 @@ export default NextAuth({
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope:'read:user'
+      scope: 'read:user'
     }),
     // ... and more providers here
   ],
+  jwt: {
+    signingKey: process.env.JWT_SIGNING_PRIVATE_KEY
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+      console.log(email)
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 })
 

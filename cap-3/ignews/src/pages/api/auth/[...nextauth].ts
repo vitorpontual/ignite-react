33e6033566806dc5f4d15e5b694d888a9f49095c
Vitor Pontual <vitorpguedes@me.com>
2021-06-22 17:47:02 -0300

    Checkout and Webhook

--- cap-3/ignews/src/pages/api/auth/[...nextauth].ts
+++ cap-3/ignews/src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,6 @@ import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import { signIn } from 'next-auth/client';
 import Providers from 'next-auth/providers';
-import { FaUntappd } from 'react-icons/fa';
 import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
@@ -15,13 +14,10 @@ export default NextAuth({
     }),
     // ... and more providers here
   ],
-  jwt: {
-    signingKey: process.env.JWT_SIGNING_PRIVATE_KEY
-  },
+  
   callbacks: {
     async signIn(user, account, profile) {
       const { email } = user
-      console.log(email)
 
       try {
         await fauna.query(

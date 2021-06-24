1d2512aed0e6f3e710a0269b2b15dbd3c4527da2
Vitor Pontual <vitorpguedes@me.com>
2021-06-24 15:56:02 -0300

    Post Preview

--- cap-3/ignews/src/components/SubscribeButton/index.tsx
+++ cap-3/ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
 import { signIn, useSession } from 'next-auth/client';
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
@@ -15,13 +16,18 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter();
 
   async function handleSubscribe(){
     if(!session) {
       signIn('github');
       return
     }
-    // criação da checkout
+
+    if(session.activeSubscription){
+      router.push('/posts')
+      return;
+    }
 
     try { 
       const response = await api.post('/subscribe')

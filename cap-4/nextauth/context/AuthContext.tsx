8d29f98c3ea385e71a2a8bb34ae7d7cf526eaa6b
Vitor Pontual <vitorpguedes@me.com>
2021-07-30 16:25:14 -0300

    JWT QUEUE LOGIC

--- cap-4/nextauth/context/AuthContext.tsx
+++ cap-4/nextauth/context/AuthContext.tsx
@@ -1,4 +1,13 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import { setCookie, parseCookies } from 'nookies'
+import Router from 'next/router'
+import { api } from "../service/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+}
 
 type SignInCredentials = {
   email: string;
@@ -6,6 +15,7 @@ type SignInCredentials = {
 }
 
 type AuthContextData = {
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   isAuthenticated: boolean;
 
@@ -17,15 +27,62 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
-export function AuthProvider({children}: AuthProviderProps) {
-  const isAuthenticated = false;
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser ] = useState<User>(null);
+  const isAuthenticated = !!user;
 
-  async function signIn({ email, password}: SignInCredentials){
-    console.log({ email, password})
+  useEffect(() => {
+    const {'nextauth.token': token} = parseCookies()
+
+    if(token){
+      api.get('/me').then(response => {
+        const { email, permissions, roles} = response.data
+
+        setUser({email, permissions, roles})
+      })
+    }
+  }, [])
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post('sessions', {
+        email,
+        password
+      })
+
+      const { token, refreshToken, permissions, roles } = response.data
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
+
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
+
+
+      //localstorage - ele não fica disponivel em outras sessões
+      //sessionstorage - não tem no lado do servidor
+      //cookies - pode ser utilizado pelo lado do browser ou servidor
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      })
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
+      Router.push('/dashboard')
+
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  return(
-    <AuthContext.Provider value={{signIn, isAuthenticated}}>
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   )
